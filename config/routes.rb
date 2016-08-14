3acb368d5d9cc2a7c6ac3caf091ffb66a64dfcc7
Railsbridge Student <unconfigured-student@railsbridge.local>
2016-08-14 12:52:11 +0000

    added post resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users
   root to: 'static#home'
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ spec/factories/posts.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :post do
+    date "2016-08-14"
+rationale "MyText"
+  end
+
+end
