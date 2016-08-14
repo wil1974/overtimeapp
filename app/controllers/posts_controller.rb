b5be2d8ad9deeb85760063d702de5210e63b0d15
Railsbridge Student <unconfigured-student@railsbridge.local>
2016-08-14 13:22:55 +0000

    added tdd for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,26 @@
 class PostsController < ApplicationController
   def index
+  end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "Post created successfully"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:date, :rationale)
   end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,7 +11,25 @@ describe 'navigate' do
     end
 
     it 'has a title of Posts' do
-      expect(page).to have_content(/Posts/)
+      expect(page).to have_content("Posts")
     end
   end
+
+  describe 'creation' do
+    before do
+      visit new_post_path
+    end
+
+    it 'has a NEW form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale text"
+      click_on "Save"
+      expect(page).to have_content("Some rationale text")
+    end
+  end
+
 end
