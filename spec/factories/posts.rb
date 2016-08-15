4af3e3e0be7ba892bce2c999ff94568c97f7bc73
Railsbridge Student <unconfigured-student@railsbridge.local>
2016-08-15 16:41:23 +0000

    Added FactoryGirl factories

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
   factory :post do
-    date "2016-08-14"
-rationale "MyText"
+    date Date.today
+    rationale "Some Rationale text"
   end
 
+  factory :second_post, class: "Post" do
+    date Date.yesterday
+    rationale "Different Rationale text"
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,17 @@
 FactoryGirl.define do
   factory :user do
-    
+    first_name 'John'
+    last_name 'Snow'
+    email 'test@test.com'
+    password 'password'
+    password_confirmation  'password'
   end
 
+  factory :admin_user, class: "AdminUser" do
+    first_name 'Admin'
+    last_name 'User'
+    email 'admin@user.com'
+    password 'password'
+    password_confirmation  'password'
+  end
 end
