73733307c3e2aa07e9390fc916bf2c39f36cd985
Railsbridge Student <unconfigured-student@railsbridge.local>
2016-09-04 18:33:28 +0000

    implement enum method for status posts for approval workflow

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
+  # enum method allows triggering of state in database
+  # so you could use p.approved!, p.submitted? etc
+  enum status: {submitted: 0, approved: 1, rejected: 2}
   belongs_to :user
-
   validates_presence_of :date, :rationale
 end
